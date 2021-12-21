294cea71d2ecb7d84f40a0a6139d86ab8808d0d5
Prajwal Joshi <prajwal.j@codilar.com>
2021-12-21 22:40:23 +0530

    Employe Task

---
+++ Controller/Employee/Add.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Codilar\Employee\Controller\Employee;
+
+use Magento\Framework\App\Action\Action;
+use Codilar\Employee\Model\EmployeeFactory as ModelFactory;
+use Codilar\Employee\Model\ResourceModel\Employee as ResourceModel;
+use Magento\Framework\App\Action\Context;
+
+class Add extends Action
+{
+    /**
+     * @var ModelFactory
+     */
+    protected $modelFactory;
+
+    /**
+     * @var ResourceModel
+     */
+    protected $resourceModel;
+
+    public function __construct(
+        Context $context,
+        ModelFactory $modelFactory,
+        ResourceModel $resourceModel
+    )
+    {
+        parent::__construct($context);
+        $this->modelFactory = $modelFactory;
+        $this->resourceModel = $resourceModel;
+    }
+
+    public function execute()
+    {
+        $data = $this->getRequest()->getParams();
+        $emptyEmployee = $this->modelFactory->create();
+        $this->resourceModel->load($emptyEmployee,$data['entity_id']);
+        $emptyEmployee->setName($data['name'] ?? null);
+        $emptyEmployee->setEmail($data['email'] ?? null);
+        $emptyEmployee->setMobile($data['mobile'] ?? null);
+        $emptyEmployee->setDob($data['dob'] ?? null);
+        $emptyEmployee->setAddress($data['address'] ?? null);
+        $emptyEmployee->setDoj($data['doj'] ?? null);
+
+        try {
+            
+            $this->resourceModel->save($emptyEmployee);
+            $this->messageManager->addSuccessMessage(__('Employee %1 saved successfully', $emptyEmployee->getName()));
+        } catch (\Exception $exception) {
+            $this->messageManager->addErrorMessage(__("Error saving Employee"));
+        }
+        
+        return $this->resultRedirectFactory->create()->setPath('employee/employee/view');
+    }
+}
